171d2c2ff71eaa7baa85bea0986634ca1ac8cc0d
Ike Esquivel-Pilloud and Nathan Powers <nathan@>
2019-10-31 11:54:42 -0700

    add funky code

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $("form#input").submit(function(event){
+  var num = $("#userInput").val();
+  event.preventDefault()
+  console.log(num)
+
+
+console.log(56);
+  var roman = function(num)
+    if (num > 4,000) {
+    alert("That's enough, young man.")
+  } else .return(num){
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+});
